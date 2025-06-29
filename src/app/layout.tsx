8dbb5f03a1f562d29a9afb1307d6fb07f0a77811
shubham-Alhat <shubhamalhat520@gmail.com>
2025-06-29 20:34:45 +0530

    all about navbar and UI.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,15 @@ export default function RootLayout({
           >
             <div className="min-h-screen">
               <Navbar />
-              {children}
+              <main className="py-8">
+                {/* container to center the content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
             </div>
           </ThemeProvider>
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-    </div>
-  );
+  return <div>home page content</div>;
 }
