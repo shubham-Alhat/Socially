55726265ced31caa90a955c0fdcfd69d026cd417
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-01 15:45:57 +0530

    broke app and going to deplot

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -56,7 +56,7 @@ export async function getUserByClerkId(clerkId: string) {
 export async function getDbUserId() {
   const { userId: clerkId } = await auth();
 
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import CreatePost from "@/components/ui/CreatePost";
 import PostCard from "@/components/ui/PostCard";
 import WhoToFollow from "@/components/ui/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
+import { getAllPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 
 export default async function Home() {
   const user = await currentUser();
   const posts = await getAllPosts();
+  const dbUserId = await getDbUserId();
+
+  console.log(posts);
 
   return (
     <>
@@ -14,7 +19,7 @@ export default async function Home() {
           {user ? <CreatePost /> : null}
           <div className="space-y-6">
             {posts.map((post) => (
-              <PostCard key={post.id} post={post} />
+              <PostCard key={post.id} post={post} dbUserId={dbUserId} />
             ))}
           </div>
         </div>
